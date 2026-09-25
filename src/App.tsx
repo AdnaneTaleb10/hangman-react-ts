8dc39f2b57b4df68d2b1847e68547875ae5b5b01
Mohamed Adnen Taleb <adnane102464@gmail.com>
2026-09-25 10:31:22 +0100

    feat: display guessed letters in hangman word

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import words from "./wordList.json";
 import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
 
 function App() {
   const [wordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
   });
-
   console.log(wordToGuess);
 
   return (
     <div className="my-0 mx-auto flex max-w-200 flex-col items-center gap-8">
       <div className="text-center text-4xl">Lose Win</div>
       <HangmanDrawing />
+      <HangmanWord />
     </div>
   );
 }
--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,1 +1,18 @@
-export function HangmanWord() {}
+export function HangmanWord() {
+  const word = "test";
+  const guessedLetter = ["t", "e"];
+
+  return (
+    <div className="flex gap-[.25em] text-8xl text-bold uppercase font-mono">
+      {word.split("").map((letter, index) => (
+        <span key={index} className="border-b-8">
+          <span
+            className={`${guessedLetter.includes(letter) ? "visible" : "invisible"}`}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
